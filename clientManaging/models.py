6da002bd2404fe3761ae6874ab2e138310738dc4
JuanLoncharich <juanandresloncha@gmail.com>
2024-05-07 08:58:31 -0300

    organizing the whole project

--- clientManaging/models.py
+++ clientManaging/models.py
@@ -2,4 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Client(models.Model):
+    idClient = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+    address = models.TextField()
--- clientManaging/views.py
+++ clientManaging/views.py
@@ -2,9 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    title = 'Django test'
-    return render(request, 'index.html', {
-        'title': title
+def addClient(request):
+    return render(request, 'clients.html', {
+        'title': 'Django test'
     })
 
